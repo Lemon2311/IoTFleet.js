4417af5ef6c8d0b982b85168c3cfbb9aa87fb67d
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-22 20:11:05 +0100

    working with digital pins fully works, configured pins dont save from session to session

--- ui/script.js
+++ ui/script.js
@@ -36,9 +36,6 @@ function changePinState(type, x, state) {
   return state;
 }
 
-changePinState("digital", 13, "off");
-
-// Function to add a new item to the list
 function addItem() {
   const typeInput = document.querySelector("#addItemSection .type");
   const pinInput = document.querySelector(
@@ -60,11 +57,45 @@ function addItem() {
       <img src="IO.svg" class="icon" />
       <span>${type}</span>
       <span>${pin}</span>
+      <img src="false.svg" class="checkmark-icon toggle-icon" onclick="toggleIcon(this)" /> <!-- Include the checkmark here -->
   `;
 
   itemList.appendChild(listItem);
 
+  changePinState(type, pin, "off");
+
   // Clear input fields
   typeInput.value = "";
   pinInput.value = "";
+}
+
+function toggleIcon(imgElement) {
+  // Find the parent list item of the clicked image
+  // This is necessary to get the context in which the image was clicked
+  const listItem = imgElement.closest('.thin-list-item');
+
+  // Extract the type and pin values from the list item
+  // These values are assumed to be in the first and second span elements respectively
+  // 'textContent' is used to get the text inside the span elements
+  const type = listItem.querySelector('span:nth-of-type(1)').textContent;
+  const pin = listItem.querySelector('span:nth-of-type(2)').textContent;
+
+  // Initialize a variable to hold the new state of the pin
+  let newState;
+
+  // Check the current source of the image to determine the toggle action
+  // If the current image is 'false.svg', switch to 'true.svg' and set the new state to 'on'
+  // If the current image is not 'false.svg', switch to 'false.svg' and set the new state to 'off'
+  if (imgElement.src.includes('false.svg')) {
+      imgElement.src = 'true.svg';
+      newState = 'on';
+  } else {
+      imgElement.src = 'false.svg';
+      newState = 'off';
+  }
+
+  // Call the changePinState function with the type, pin, and the new state
+  // This function is expected to handle the logic of changing the state of the pin
+  // The exact behavior of this function depends on its implementation which is not shown here
+  changePinState(type, pin, newState);
 }
