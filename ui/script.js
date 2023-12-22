4d8a8b2474cd097c8b62df880876c639b1049854
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-22 18:52:51 +0100

    ui function for calling esp32 digitalPins state change initialized

--- ui/script.js
+++ ui/script.js
@@ -14,3 +14,25 @@
 //         });
 //     });
 // });
+
+function switchDX(x,state) {
+  fetch('http://192.168.1.243/d'+x+'/'+state)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text(); // You can change this to response.json() if the server returns JSON.
+    })
+    .then(data => {
+      console.log('Response:', data);
+      // You can add further handling here if needed.
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Handle errors here, e.g., display an error message to the user.
+    });
+
+    return state
+}
+
+switchDX(13,"off");
