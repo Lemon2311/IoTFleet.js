e0fb7b36b9dab99a70f5797fd4f5074efcfb20a2
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-22 15:28:25 +0100

    ui tweaks

--- ui/script.js
+++ ui/script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".list-item");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const items = document.querySelectorAll(".list-item");
 
-    items.forEach(item => {
-        const header = item.querySelector(".item-header");
-        const content = item.querySelector(".item-content");
+//     items.forEach(item => {
+//         const header = item.querySelector(".item-header");
+//         const content = item.querySelector(".item-content");
 
-        header.addEventListener("click", () => {
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
-    });
-});
+//         header.addEventListener("click", () => {
+//             if (content.style.maxHeight) {
+//                 content.style.maxHeight = null;
+//             } else {
+//                 content.style.maxHeight = content.scrollHeight + "px";
+//             }
+//         });
+//     });
+// });
