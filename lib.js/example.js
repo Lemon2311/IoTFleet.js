455e72b2253a02daf1df409dcfb0bf4733129e6b
Lemon2311 <marcotighiliu@yahoo.com>
2024-01-02 18:31:04 +0200

    updated example to use proper esp32 pins

--- lib.js/example.js
+++ lib.js/example.js
@@ -1,9 +1,9 @@
 import { output, input } from "./script.js";
 
-const { d13, a27 } = output("192.168.1.138", "d13", "a27");
+const { d13, a25 } = output("192.168.1.138", "d13", "a25");
 
 d13.set("high");
-a27.set(2);
+a25.set(2);
 
 const { d3, a34 } = input("192.168.1.138", "d3", "a34");
 
