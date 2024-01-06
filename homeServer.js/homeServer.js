e0284a4232870266e8cbb85ebbdf1c349cff67ba
Lemon2311 <marcotighiliu@yahoo.com>
2024-01-06 08:08:25 +0100

    working on analog output & inputs

--- homeServer.js/homeServer.js
+++ homeServer.js/homeServer.js
@@ -1,57 +1,92 @@
-import {output} from '../lib.js/script.js';
+import { output, input } from "../lib.js/script.js";
 
 // Get the add-item button element
-const addItemButton = document.getElementById('add');
+const addItemButton = document.getElementById("add");
 
 // Get the list element
-const itemList = document.getElementById('item-list');
+const itemList = document.getElementById("item-list");
 
 // Get the input element
-const inputField = document.getElementById('new-item');
+const inputField = document.getElementById("new-item");
 
-// Create an array to store the digital outputs
+// Create an array to store the outputs
 let outputs = [];
+
+// Create an array to store the inputs
+let inputs = [];
 
 // Function to handle the add-item button click event
 function handleAddItemClick() {
-    // Get the value of the input field
-    const newItemText = inputField.value;
-    
-    // Get the index of the first '/' character
-    const slashIndex = newItemText.indexOf('/');
-    
-    // Get the text before the '/'
-    const beginningText = newItemText.substring(0, slashIndex);
+  // Get the value of the input field
+  const newItemText = inputField.value;
 
-    // Clear the input field
-    inputField.value = '';
+  // Get the index of the first '/' character
+  const slashIndex = newItemText.indexOf("/");
 
-    // Create a new button element
-    const newItem = document.createElement('button');
-    
-    // Set the text of the button
-    newItem.textContent = newItemText.substring(slashIndex+1, newItemText.length);
+  //Get the text after the '/' character
+  const afterSlashText = newItemText.substring(
+    slashIndex + 1,
+    newItemText.length
+  );
 
-    newItem.id = outputs.length;
+  // Get the text before the '/'
+  const beginningText = newItemText.substring(0, slashIndex);
 
-    // Add the button to the list
-    itemList.appendChild(newItem);
+  //Get pin type
+  const type = beginningText.substring(0, 1);
 
-    // Add the output to the outputs array
-    outputs.push(output("192.168.1.138", beginningText)[beginningText]) - 1;
+  // Clear the input field
+  inputField.value = "";
 
-    // Add a click event listener to the new button, to toggle the output
-    let clickCount = 0;
-    newItem.addEventListener('click', function() {
-        clickCount++;
-        if (clickCount % 2 === 1) {
-            outputs[newItem.id].set("high");
-        } else {
-            outputs[newItem.id].set("low");
-        }
-    });
+  // Create a new button element
+  const newItem = document.createElement("button");
+
+  if (type == "a") {
+    var inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.placeholder = "?Volts";
+    // Add the input field to the button
+    newItem.appendChild(inputField);
+  }
+
+  // Set the text of the button
+  newItem.textContent = afterSlashText.substring(
+    0,
+    afterSlashText.indexOf("/")
+  );
+
+  newItem.id = outputs.length;
+
+  // Add the button to the list
+  itemList.appendChild(newItem);
+
+  // Add the output to the outputs array
+  outputs.push(output("192.168.1.138", beginningText)[beginningText]) - 1;
+
+  if (type == "d") handleDigitalOutput(newItem);
+  else handleAnalogOutput(newItem, inputField.value);
 }
 
-// Add a click event listener to the add-item button
-addItemButton.addEventListener('click', handleAddItemClick);
+const handleAnalogOutput = (newItem, value) => {
+  outputs[newItem.id].set(value);
+};
 
+const handleDigitalOutput = (newItem) => {
+  // Add a click event listener to the new button, to toggle the output
+  let clickCount = 0;
+  newItem.addEventListener("click", function () {
+    clickCount++;
+    if (clickCount % 2 === 1) {
+      outputs[newItem.id].set("high");
+    } else {
+      outputs[newItem.id].set("low");
+    }
+  });
+};
+
+const handleDigitalInput = (pin) => {
+  return input("192.168.1.138", pin).get();
+};
+
+// Add a click event listener to the add-item button
+addItemButton.addEventListener("click", handleAddItemClick);
