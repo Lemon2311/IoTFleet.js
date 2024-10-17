4e8792ed67b684c8e695f1ece3611f36de54b418
Lemon2311 <marcotighiliu@yahoo.com>
2024-10-17 20:22:30 +0300

    workProg3

--- IoTFleet.py
+++ IoTFleet.py
@@ -1,14 +1,15 @@
 import requests
 
-class Output:
-    def __init__(self, type, pin, ip):
+class IO:
+    def __init__(self, mode, type, pin, ip):
+        self.mode = mode
         self.type = type
         self.pin = pin
         self.ip = ip
 
         response = requests.post(
             f"http://{self.ip}/initPin", 
-            params={'type': self.type, 'pin': self.pin, 'mode': 'OUT'}
+            params={'type': self.type, 'pin': self.pin, 'mode': self.mode}
         )
         
         if response.status_code != 200:
@@ -18,6 +19,8 @@ class Output:
 
     def __setattr__(self, name, value):
         if name == 'state':
+            value = int(value)
+
             response = requests.post(
                 f"http://{self.ip}/setPin", 
                 params={'type': self.type, 'pin': self.pin, 'state': value}
@@ -25,7 +28,29 @@ class Output:
             
             if response.status_code != 200:
                 raise Exception(f"Failed to set state {value} for pin {self.pin}. Status code: {response.status_code}")
-        
+            
+            self.__dict__['state'] = value
             print(response.text)
         else:
             super().__setattr__(name, value)
+
+    def __str__(self):
+        if self.mode == 'IN':
+            response = requests.get(
+                f"http://{self.ip}/pinState", 
+                params={'type': self.type, 'pin': self.pin}
+            )
+            
+            if response.status_code != 200:
+                raise Exception(f"Failed to get state for pin {self.pin}. Status code: {response.status_code}")
+            
+            state = int(response.text)
+        elif self.mode == 'OUT':
+            state = self.__dict__.get('state', 0)
+        else:
+            state = 0
+
+        return str(state)
+
+    def __int__(self):
+        return self.__dict__.get('state', 0)
